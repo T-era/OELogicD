8caf17495fd92c78ccb005977c439d2ba58af6c5
t-era <t-era@ray.ocn.ne.jp>
2015-01-18 10:39:07 +0900

    Resolver と Quest の役割見直し

--- src/Quest.d
+++ src/Quest.d
@@ -7,10 +7,26 @@ import std.stdio;
 class Quest {
 	int[][] vHints;
 	int[][] hHints;
+	int width;
+	int height;
+	Cell[][] cells;
 
 	this(int[][] vHints, int[][] hHints) {
 		this.vHints = vHints;
 		this.hHints = hHints;
+
+		this.width = vHints.length;
+		this.height = hHints.length;
+		this.cells.length = this.height;
+		for (int y = 0; y < this.height; y ++) {
+			cells[y].length = this.width;
+			for (int x = 0; x < this.width; x ++) {
+				cells[y][x] = Cell.Unknown;
+			}
+		}
+		if (sumOf(vHints) != sumOf(hHints)) {
+			throw new Exception("??");
+		}
 	}
 	this(string[] vHints, string[] hHints, string separator=",") {
 		this.vHints = map!(
@@ -19,6 +35,49 @@ class Quest {
 		this.hHints = map!(
 				str => map!(to!int)(str.split(separator)).array()
 			)(hHints).array();
+
+		this.width = vHints.length;
+		this.height = hHints.length;
+		this.cells.length = this.height;
+		for (int y = 0; y < this.height; y ++) {
+			cells[y].length = this.width;
+			for (int x = 0; x < this.width; x ++) {
+				cells[y][x] = Cell.Unknown;
+			}
+		}
+	}
+
+	private static int sumOf(int[][] args) {
+		int sum = 0;
+		for (int i = 0; i < args.length; i ++) {
+			for (int j = 0; j < args[i].length; j ++) {
+				sum += args[i][j];
+			}
+		}
+		return sum;
+	}
+	override string toString() {
+		writeln(cells);
+		string str = "";
+		for (int y = 0; y < cells.length; y ++) {
+			for (int x = 0; x < cells[y].length; x ++) {
+				switch(cells[y][x]) {
+					case Cell.Unknown:
+						str ~= ".";
+						break;
+					case Cell.Empty:
+						str ~= "X";
+						break;
+					case Cell.Fill:
+						str ~= " ";
+						break;
+					default:
+						throw new Exception("??");
+				}
+			}
+			str ~= "\n";
+		}
+		return str;
 	}
 }
 
--- src/extent_resolver/Resolver.d
+++ src/extent_resolver/Resolver.d
@@ -8,38 +8,28 @@ private import parts.ExclusiveException;
 private import parts.Position;
 
 class Resolver {
-	private int[][] xHints;
-	private int[][] yHints;
-	private int width;
-	private int height;
-	private Cell[][] cells;
+	private Quest quest;
 	private LinePossibility[] vPossibility;
 	private LinePossibility[] hPossibility;
 
 	this(Quest quest) {
-		this.width = quest.vHints.length;
-		this.height = quest.hHints.length;
+		this.quest = quest;
 
-		vPossibility.length = width;
-		hPossibility.length = height;
-		for (int x = 0; x < width; x ++) {
-			vPossibility[x] = new LinePossibility(this, height, quest.vHints[x], this.verticalCallback(x), this.getCellAtX(x));
+		vPossibility.length = quest.width;
+		hPossibility.length = quest.height;
+		for (int x = 0; x < quest.width; x ++) {
+			vPossibility[x] = new LinePossibility(this
+				, quest.height
+				, quest.vHints[x]
+				, this.verticalCallback(x)
+				, this.getCellAtX(x));
 		}
-		for (int y = 0; y < height; y ++) {
-			hPossibility[y] = new LinePossibility(this, width, quest.hHints[y], this.horizontalCallback(y), this.getCellAtY(y));
-		}
-
-		int xSum = 0;
-		int ySum = 0;
-		cells.length = this.height;
-		for (int y = 0; y < this.height; y ++) {
-			cells[y].length = this.width;
-			for (int x = 0; x < this.width; x ++) {
-				cells[y][x] = Cell.Unknown;
-			}
-		}
-		if (sumOf(xHints) != sumOf(yHints)) {
-			throw new Exception("??");
+		for (int y = 0; y < quest.height; y ++) {
+			hPossibility[y] = new LinePossibility(this
+				, quest.width
+				, quest.hHints[y]
+				, this.horizontalCallback(y)
+				, this.getCellAtY(y));
 		}
 	}
 	public void checkUp() {
@@ -53,15 +43,15 @@ class Resolver {
 
 	public auto verticalCallback(int x) {
 		void _inner(Cell c, int y) {
-			if (0 <= x && x < width
-				&& 0 <= y && y < height) {
-				if (cells[y][x] != c) {
-					if (cells[y][x] != Cell.Unknown) {
+			if (0 <= x && x < quest.width
+				&& 0 <= y && y < quest.height) {
+				if (quest.cells[y][x] != c) {
+					if (quest.cells[y][x] != Cell.Unknown) {
 						throw new ExclusiveException(
 							Position(x, y),
-							format("Put on anothervalue on (%d, %d): %s-> %s", x, y, cells[y][x], c));
+							format("Put on anothervalue on (%d, %d): %s-> %s", x, y, quest.cells[y][x], c));
 					}
-					cells[y][x] = c;
+					quest.cells[y][x] = c;
 					hPossibility[y].set(c, x);
 					hPossibility[y].checkUp();
 				}
@@ -71,15 +61,15 @@ class Resolver {
 	}
 	public auto horizontalCallback(int y) {
 		void _inner(Cell c, int x) {
-			if (0 <= x && x < width
-				&& 0 <= y && y < height) {
-				if (cells[y][x] != c) {
-					if (cells[y][x] != Cell.Unknown) {
+			if (0 <= x && x < quest.width
+				&& 0 <= y && y < quest.height) {
+				if (quest.cells[y][x] != c) {
+					if (quest.cells[y][x] != Cell.Unknown) {
 						throw new ExclusiveException(
 							Position(x, y),
-							format("Put on anothervalue on (%d, %d): %s-> %s", x, y, cells[y][x], c));
+							format("Put on anothervalue on (%d, %d): %s-> %s", x, y, quest.cells[y][x], c));
 					}
-					cells[y][x] = c;
+					quest.cells[y][x] = c;
 					vPossibility[x].set(c, y);
 					vPossibility[x].checkUp();
 				}
@@ -90,9 +80,9 @@ class Resolver {
 
 	public auto getCellAtX(int x) {
 		Cell _inner(int y) {
-			if (0 <= y && y < height
-				&& 0 <= x && x < width) {
-				return cells[y][x];
+			if (0 <= y && y < quest.height
+				&& 0 <= x && x < quest.width) {
+				return quest.cells[y][x];
 			} else {
 				return Cell.Unknown;
 			}
@@ -101,9 +91,9 @@ class Resolver {
 	}
 	public auto getCellAtY(int y) {
 		Cell _inner(int x) {
-			if (0 <= y && y < height
-				&& 0 <= x && x < width) {
-				return cells[y][x];
+			if (0 <= y && y < quest.height
+				&& 0 <= x && x < quest.width) {
+				return quest.cells[y][x];
 			} else {
 				return Cell.Unknown;
 			}
@@ -112,45 +102,14 @@ class Resolver {
 	}
 
 	override string toString() {
-		string str = "";
-		for (int y = 0; y < cells.length; y ++) {
-			for (int x = 0; x < cells[y].length; x ++) {
-				switch(cells[y][x]) {
-					case Cell.Unknown:
-						str ~= ".";
-						break;
-					case Cell.Empty:
-						str ~= " ";
-						break;
-					case Cell.Fill:
-						str ~= "X";
-						break;
-					default:
-						throw new Exception("??");
-				}
-			}
-			str ~= "\n";
+		string str = "||";
+		foreach (LinePossibility lb; vPossibility) {
+			str ~= lb.toString();
+		}
+		str ~= "=";
+		foreach (LinePossibility lb; hPossibility) {
+			str ~= lb.toString();
 		}
 		return str;
-	}
-	public void showDetail() {
-		writeln("||");
-		foreach (LinePossibility lb; vPossibility) {
-			writeln(lb);
-		}
-		writeln("=");
-		foreach (LinePossibility lb; hPossibility) {
-			writeln(lb);
-		}
-	}
-
-	private static int sumOf(int[][] args) {
-		int sum = 0;
-		for (int i = 0; i < args.length; i ++) {
-			for (int j = 0; j < args[i].length; j ++) {
-				sum += args[i][j];
-			}
-		}
-		return sum;
 	}
 }
